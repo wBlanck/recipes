5560ef4e5d9e7066e4f36a3f49baaeaea028a9f4
wblanck <blanck.william@gmail.com>
2022-02-10 15:27:46 +0100

    added recipeList compt

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,8 +1,19 @@
+import { useFetch } from "../../hooks/useFetch";
+//Styles
 import "./Home.css";
 
+//Components
+import RecipeList from "../../components/RecipeList";
+
 function Home() {
-  const { data, isPending, error } = useFetch("");
-  return <div>Home</div>;
+  const { data, isPending, error } = useFetch("http://localhost:3000/recipes");
+  return (
+    <div className="home">
+      {error && <p className="error">{error}</p>}
+      {isPending && <p className="loading">Loading...</p>}
+      {data && <RecipeList recipes={data} />}
+    </div>
+  );
 }
 
 export default Home;
