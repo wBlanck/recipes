a47adda2f7f6f365d9732230ce9e05b248e49599
wblanck - laptop <blanck.william@gmail.com>
2022-02-10 12:30:09 +0100

    navbar styles

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import Home from "./pages/home/Home";
 import Create from "./pages/create/Create";
 import Recipe from "./pages/recipe/Recipe";
 import Search from "./pages/search/Search";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+        <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/create" element={<Create />} />
