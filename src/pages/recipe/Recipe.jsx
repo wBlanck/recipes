b65830609d167a9e071c3d466397d1c81d1936fa
wblanck - laptop <blanck.william@gmail.com>
2022-02-10 12:18:05 +0100

    added pages

---
+++ src/pages/recipe/Recipe.jsx
@@ -0,0 +1,7 @@
+import "./Recipe.css";
+
+function Recipe() {
+  return <div>Recipe</div>;
+}
+
+export default Recipe;
---
+++ src/pages/search/Search.jsx
@@ -0,0 +1,7 @@
+import "./Search.css";
+
+function Search() {
+  return <div>Search</div>;
+}
+
+export default Search;
