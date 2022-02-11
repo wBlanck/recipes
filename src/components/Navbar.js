1ada93fb473dee38e8bcade311fd64a44d2ee8c9
wblanck <blanck.william@gmail.com>
2022-02-11 10:04:33 +0100

    added ThemeContext

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,13 @@
 import { Link } from "react-router-dom";
+import { useTheme } from "../hooks/useTheme";
 import "./Navbar.css";
 import SearchBar from "./SearchBar";
 
 function Navbar() {
+  const { color } = useTheme();
+
   return (
-    <div className="navbar">
+    <div className="navbar" style={{ background: color }}>
       <nav>
         <Link to="/" className="brand">
           <h1>Cooking</h1>
