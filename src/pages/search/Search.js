444134b6c2aa1a4edd31aea6c1ab9d2e263c710a
wblanck <blanck.william@gmail.com>
2022-02-11 08:34:46 +0100

    added search logic

--- src/pages/search/Search.js
+++ src/pages/search/Search.js
@@ -1,7 +1,26 @@
+import { useLocation } from "react-router-dom";
+import { useFetch } from "../../hooks/useFetch";
+
+import RecipeList from "../../components/RecipeList";
+
 import "./Search.css";
 
 function Search() {
-  return <div>Search</div>;
+  const queryString = useLocation().search;
+  const queryParams = new URLSearchParams(queryString);
+  const query = queryParams.get("q");
+
+  const url = "http://localhost:3000/recipes?q=" + query;
+  const { error, isPending, data } = useFetch(url);
+
+  return (
+    <div>
+      <h2 className="page-title">Recipes including "{query}"</h2>
+      {error && <p className="error">{error}</p>}
+      {isPending && <p className="loading">Loading...</p>}
+      {data && <RecipeList recipes={data} />}
+    </div>
+  );
 }
 
 export default Search;
