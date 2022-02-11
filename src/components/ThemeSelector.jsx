da819e3de9421cad4cc616a972a7e4ede58fc111
wblanck - laptop <blanck.william@gmail.com>
2022-02-11 11:52:33 +0100

    added toggle mode button

--- src/components/ThemeSelector.jsx
+++ src/components/ThemeSelector.jsx
@@ -1,11 +1,27 @@
 import { useTheme } from "../hooks/useTheme";
 import "./ThemeSelector.css";
+import modeIcon from "../assets/mode-icon.svg";
+
+const themeColors = ["#58249c", "#249c6b", "#b70233"];
 
 function ThemeSelector() {
-  const { changeColor } = useTheme();
-  const themeColors = ["#58249c", "#249c6b", "#b70233"];
+  const { changeColor, changeMode, mode } = useTheme();
+
+  const toggleMode = () => {
+    /* mode === "dark" ? changeMode("light") : changeMode("dark"); */
+    changeMode(mode === "dark" ? "light" : "dark");
+  };
+  console.log(mode);
   return (
     <div className="theme-selector">
+      <div className="mode-toggle">
+        <img
+          src={modeIcon}
+          alt="dark/light toggle icon"
+          onClick={toggleMode}
+          style={{ filter: mode === "dark" ? "invert(100%)" : "invert(20%)" }}
+        />
+      </div>
       <div className="theme-buttons">
         {themeColors.map((color) => (
           <div
